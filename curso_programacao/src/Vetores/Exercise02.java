e531d172e669386414ec870894ea18605e5b372e
Pedrohss <116673382+Pedrohss2@users.noreply.github.com>
2023-05-05 18:14:58 -0300

    Exercícios vetores resolvidos

---
+++ curso_programacao/src/Vetores/Exercise02.java
@@ -0,0 +1,45 @@
+package Vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida:
+//- Imprimir todos os elementos do vetor
+//- Mostrar na tela a soma e a média dos elementos do vetor
+public class Exercise02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int n;
+        double soma, media;
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        n = sc.nextInt();
+
+        double[] vetor = new double[n];
+
+        for (int i=0; i<n; i++) {
+            System.out.print("Digite um numero: ");
+            vetor[i] = sc.nextDouble();
+        }
+
+        soma = 0;
+        for (int i=0; i<n; i++) {
+            soma = soma + vetor[i];
+        }
+
+        media = soma / n;
+
+        System.out.print("VALORES = ");
+
+        for (int i=0; i<n; i++) {
+            System.out.printf("%.1f  ", vetor[i]);
+        }
+
+        System.out.printf("\nSOMA = %.2f\n", soma);
+        System.out.printf("MEDIA = %.2f\n", media);
+
+        sc.close();
+    }
+}
+
