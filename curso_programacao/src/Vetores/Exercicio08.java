5ca3e631918a3c49146b2521ebb29e453ef441c7
Pedrohss <116673382+Pedrohss2@users.noreply.github.com>
2023-05-08 17:53:25 -0300

    all right

--- curso_programacao/src/Vetores/Exercicio08.java
+++ curso_programacao/src/Vetores/Exercicio08.java
@@ -10,7 +10,31 @@ public class Exercicio08 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        double numeroPares = 0, quantidade = 0;
 
-        //...........
+        System.out.println("Quantos elemntos vai ter o vetor?");
+        int elementos = sc.nextInt();
+
+        double[] numero = new double[elementos];
+
+        for(int i = 0; i<numero.length;i++){
+            System.out.println("Digite um numero");
+            numero[i] = sc.nextDouble();
+        }
+
+        for(int i = 0; i<numero.length;i++){
+            if(numero[i] %2 == 0){
+                quantidade = quantidade + numero[i];
+                numeroPares++;
+            }
+        }
+        if(numeroPares != 0) {
+            double media = quantidade / numeroPares;
+            System.out.printf("Media %.2f%n", media);
+        }
+        else {
+            System.out.println("Nem um numero PAR.");
+        }
+        sc.close();
     }
 }
